dab9f046cbcda65b7adea500177eeaa4e269c731
Алексей Дмитриев <92928918+AlekseyAD@users.noreply.github.com>
2022-07-27 23:33:54 +0300

    Доработка задач

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,34 +1,56 @@
-let hasTooltip = document.getElementsByClassName("has-tooltip");
-hasTooltip = Array.from(hasTooltip);
+// !Вариант 3
+let hasTooltip = Array.from(document.getElementsByClassName("has-tooltip"));
+const tooltipNewDiv = document.createElement("div");
 
 hasTooltip.forEach((item) => {
-  const tooltipDiv = document.createElement("div");
-  tooltipDiv.innerText = `${item.title}`;
+  tooltipNewDiv.innerText = `${item.title}`;
   item.addEventListener("click", (event) => {
     event.preventDefault();
+    tooltipNewDiv.classList.add("tooltip");
     let tooltipPlace = item.getBoundingClientRect();
-    tooltipDiv.setAttribute("id", "idr");
-    tooltipDiv.classList.add("tooltip");
-    tooltipDiv.setAttribute(
-      `style`,
-      `left: ${tooltipPlace.left + 10}px; top: ${tooltipPlace.top + 22}px`
-    );
-    item.insertAdjacentElement("afterend", tooltipDiv);
-    const tooltipActive = Array.from(
-      document.querySelectorAll(".tooltip_active")
-    );
-    const findItemActive = tooltipActive.find((tooltipDiv) =>
-      tooltipDiv.classList.contains("tooltip_active")
-    );
-
-    if (findItemActive === undefined) {
-      tooltipDiv.classList.add("tooltip_active");
-    } else {
-      findItemActive.classList.remove("tooltip_active");
+    tooltipNewDiv.style.top = String(tooltipPlace.bottom) + "px";
+    tooltipNewDiv.style.left = String(tooltipPlace.left) + "px";
+    item.insertAdjacentElement("afterend", tooltipNewDiv);
+    if (!tooltipNewDiv.classList.contains("tooltip_active")) {
+      tooltipNewDiv.classList.add("tooltip_active");
     }
   });
 });
 
+// !Вариант 2 - Не верная работа подсказок при клике
+// let hasTooltip = document.getElementsByClassName("has-tooltip");
+// hasTooltip = Array.from(hasTooltip);
+
+// hasTooltip.forEach((item) => {
+//   const tooltipDiv = document.createElement("div");
+//   tooltipDiv.innerText = `${item.title}`;
+//   item.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     let tooltipPlace = item.getBoundingClientRect();
+//     tooltipDiv.setAttribute("id", "idr");
+//     tooltipDiv.classList.add("tooltip");
+//     tooltipDiv.setAttribute(
+//       `style`,
+//       `left: ${tooltipPlace.left + 10}px; top: ${tooltipPlace.top + 22}px`
+//     );
+//     item.insertAdjacentElement("afterend", tooltipDiv);
+//     const tooltipActive = Array.from(
+//       document.querySelectorAll(".tooltip_active")
+//     );
+//     const findItemActive = tooltipActive.find((tooltipDiv) =>
+//       tooltipDiv.classList.contains("tooltip_active")
+//     );
+
+//     if (findItemActive === undefined) {
+//       tooltipDiv.classList.add("tooltip_active");
+//     } else {
+//       findItemActive.classList.remove("tooltip_active");
+//     }
+//   });
+// });
+
+
+// !Вариант 1 Обновление всей странице при клике
 // hasTooltip.forEach(function (item) {
 //   item.onclick = function () {
 //     let tooltipPlace = item.getBoundingClientRect();
